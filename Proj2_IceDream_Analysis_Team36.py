4328e3a130cac97554b9987772f393dd2dcdd6db
cpisciottadeveloping <38054839+cpisciottadeveloping@users.noreply.github.com>
2019-03-25 13:19:45 -0400

    First Commit

---
+++ Proj2_IceDream_Analysis_Team36.py
@@ -0,0 +1,42 @@
+# Project 2: Providing Access to Clean Water
+# File: project_2.py
+# Date: 25 March 2019
+# By: Jalen Faulcon
+# jfaulcon
+# Charles Pisciotta
+# cpisciot
+# Sean Renwick
+# srenwick
+# Jared Edwards
+# edwar
+# Section: 2
+# Team: 36
+#
+# ELECTRONIC SIGNATURE
+# JALEN FAULCON
+# CHARLES PISCIOTTA
+# SEAN RENWICK
+# JARED EDWARDS
+# Full Name team member 4
+#
+# The electronic signatures above indicate that the program
+# submitted for evaluation is the combined effort of all
+# team members and that each member of the team was an
+# equal participant in its creation. In addition, each
+# member of the team has a general understanding of
+# all aspects of the program development and execution.
+#
+# A BRIEF DESCRIPTION OF WHAT THE PROGRAM OR FUNCTION DOES
+
+def determineDist():
+    # This function determines the distance from the start and end points
+    dist = int(input('What is the total distance between the ' +
+        'start and end points (in miles): '))
+    return dist
+
+def main():
+    # TODO: Call all functions here
+    determineDist()
+
+if __name__ == '__main__':
+    main()
